0cbe451002da604a722c82debf02a5ba4a1ddfb5
SudeepGowda55 <ssudeepgowda55@gmail.com>
2022-12-25 18:49:02 +0530

    flask frontend

--- flask_web_application/client/auth.py
+++ flask_web_application/client/auth.py
@@ -1,11 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
-@auth.route('/sign-up')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return "<h1> Sign Up </h1>"
+    if request.method == 'POST':
+        passwd = request.form.get('password')
+        if len(passwd) < 3 :
+            flash('Please Enter a password of minimum 5 characters', category='error')
+        else:
+            print(passwd)
+    return render_template("signup.html")
